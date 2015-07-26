85d42ab470e524aa174f23079cad634d06de2645
Kevin Liu <nivekuil@ucsd.edu>
2015-07-26 11:28:32 -0700

    Spawn daemon to handle playback

--- amp/amp.py
+++ amp/amp.py
@@ -1,35 +1,56 @@
 #!/usr/bin/env python
-import urllib.request, urllib.parse
-import os, sys, re, subprocess
-from signal import SIGTERM
+import os
+import sys
+import re
+import subprocess
+import urllib.request
+import urllib.parse
+from player import Player
+from util import kill_process_tree
+
+USAGE = """Usage: amp [SEARCH TERMS]
+Pass search terms to YouTube and play the first result in a background process.
+Call again with no arguments to stop playback.
+"""
+
 
 def main():
-  pidfile = "/tmp/imp.pid"
+    pidfile = "/tmp/amp.pid"
 
-  # Take each command line argument and join as a string
-  input = "".join(sys.argv[1:])
+    # Take each command line argument and join as a string
+    input = "".join(sys.argv[1:])
 
-  if not input:
-    with open(pidfile, 'r') as f:
-      pid = int(f.read())
-      try:
-        os.kill(pid, SIGTERM)
+    # If program is called with no input, then try to stop playback.
+    if not input:
+        try:
+            with open(pidfile, 'r') as f:
+                # Read the pidfile to get the pid of the process to kill.
+                pid = int(f.read().strip())
+
+        except:
+            # If the pidfile doesn't exist, then playback is not happening.
+            # Print the usage message and return.
+            print(USAGE)
+            return
+
+        # If the pidfile did exist, kill the process and all of its children
+        # processes, and then remove the pidfile.
+        kill_process_tree(pid)
+        os.remove(pidfile)
         print("Playback stopped.")
-      except:
-        print("Usage: blah blah.")
-      return
 
-  query_string = urllib.parse.urlencode({"search_query" : input})
-  html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-  search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-  url = "http://www.youtube.com/watch?v=" + search_results[0]
-  print("Now playing: " + url)
+        return
 
-  with open(pidfile, 'w') as f:
+    query_string = urllib.parse.urlencode({"search_query": input})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" +
+                                          query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})',
+                                html_content.read().decode())
 
-    player = subprocess.Popen(['mpv', url, "--no-video", "--really-quiet"])
-    print(player.pid, end='', file=f)
+    url = "http://www.youtube.com/watch?v=" + search_results[0]
 
+    player = Player(pidfile, url)
+    player.start()
 
 if __name__ == "__main__":
-  main()
+    main()
