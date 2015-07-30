f3aba4cd9246f030b256084ff2a3547c06186937
Kevin Liu <nivekuil@ucsd.edu>
2015-07-29 20:24:14 -0700

    Fix killing parent process within loop

--- amp/util.py
+++ amp/util.py
@@ -1,7 +1,7 @@
 import psutil
 
 def kill_process_tree(parent_pid):
-  parent = psutil.Process(parent_pid)
-  for child in parent.children(recursive=True):
-    child.kill()
+    parent = psutil.Process(parent_pid)
+    for child in parent.children(recursive=True):
+        child.kill()
     parent.kill()
