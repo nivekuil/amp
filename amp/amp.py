eaa27c72e82134e8738464feecc184cb964a6675
Kevin Liu <nivekuil@ucsd.edu>
2015-08-08 04:13:00 -0700

    Use https instead of http

--- amp/amp.py
+++ amp/amp.py
@@ -64,13 +64,13 @@ def main():
 
     query_string = urllib.parse.urlencode({"search_query": input})
 
-    html_content = urllib.request.urlopen("http://www.youtube.com/results?" +
+    html_content = urllib.request.urlopen("https://www.youtube.com/results?" +
                                           query_string)
 
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})',
                                 html_content.read().decode())
 
-    url = "http://www.youtube.com/watch?v=" + search_results[0]
+    url = "https://www.youtube.com/watch?v=" + search_results[0]
     player = Player(pidfile, url,
                     show_video=args[0].v, verbose=args[0].verbose)
     player.start()
