d96609d6987f2a8a6e348cb486af0a61d99e1033
Kevin Liu <nivekuil@ucsd.edu>
2015-10-09 19:03:46 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amp-player',
-    version='0.1.4',
+    version='0.1.5',
     description='Asynchronous command-line YouTube interface',
     keywords=["music", "audio", "video", "stream", "youtube"],
     url='https://github.com/nivekuil/amp',
